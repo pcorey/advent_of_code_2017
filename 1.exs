65bcf638f09763897489b568b67bd75f23694226
Pete Corey <petecorey@gmail.com>
2017-12-02 09:47:37 -0500

    Part 2

--- 1.exs
+++ 1.exs
@@ -1,11 +1,27 @@
 # http://adventofcode.com/2017/day/1
 
-System.argv
+sequence = System.argv
 |> List.first
 |> String.split("", trim: true)
-|> (fn [hd | tl] -> [hd | tl] ++ [hd] end).()
 |> Enum.map(&String.to_integer/1)
+
+# Part One
+sequence
+|> (fn [hd | tl] -> [hd | tl] ++ [hd] end).()
 |> Enum.chunk_every(2, 1, :discard)
 |> Enum.filter(fn [a | [b | []]] -> a == b end)
 |> Enum.reduce(0, fn ([n | _], sum) -> sum + n end)
+|> IO.inspect
+
+# Part Two
+sequence
+|> Enum.with_index
+|> Enum.map(fn
+  {part, index} ->
+    sequence_length = length(sequence)
+    offset = div(sequence_length, 2)
+    {part, Enum.at(sequence, rem(index + offset, sequence_length))}
+end)
+|> Enum.filter(fn {a, b} -> a == b end)
+|> Enum.reduce(0, fn ({n, _}, sum) -> sum + n end)
 |> IO.inspect
