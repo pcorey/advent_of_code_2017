61464b172f150949ba7178628c8a8bbbd46f042a
Pete Corey <petecorey@gmail.com>
2017-12-05 10:17:50 -0500

    Faster

--- 05/05.exs
+++ 05/05.exs
@@ -1,17 +1,25 @@
 defmodule Maze do
-  def stream(jumps) do
-    Stream.unfold({0, jumps}, &next/1)
+  def stream(jumps, offset) do
+    Stream.unfold({0, build_map(jumps), offset}, &next/1)
   end
 
-  def next({index, jumps}) when index >= length(jumps), do: nil
-
-  def next({index, jumps}) do
-    jump = Enum.at(jumps, index)
-    {jump, {index + jump, List.replace_at(jumps, index, jump + offset(jump))}}
+  defp build_map(jumps) do
+    jumps
+    |> Enum.with_index
+    |> Enum.map(fn {v, i} -> {i, v} end)
+    |> Enum.into(%{})
   end
 
-  def offset(jump) when jump >= 3, do: -1
-  def offset(_), do: 1
+  def next({index, jumps, offset}) do
+    case jumps[index] do
+      nil ->
+        nil
+      jump ->
+        next = Map.put(jumps, index, jump + offset.(jump))
+        {jump, {index + jump, next, offset}}
+    end
+  end
+
 end
 
 input = System.argv
@@ -19,8 +27,21 @@ input = System.argv
 |> String.split("\n", trim: true)
 |> Enum.map(&String.to_integer/1)
 
+# Part one
 input
-|> Maze.stream
+|> Maze.stream(fn _ -> 1 end)
+|> Enum.to_list
+|> length
+|> IO.inspect
+
+# Part two
+input
+|> Maze.stream(fn
+  n when n >= 3 ->
+    -1
+  _ ->
+    1
+end)
 |> Enum.to_list
 |> length
 |> IO.inspect
