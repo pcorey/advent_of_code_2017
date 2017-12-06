3eec3ceb8156a98cb3ac5615cf5ee16400bedce5
Pete Corey <petecorey@gmail.com>
2017-12-06 09:34:25 -0500

    Part two

--- 06/06.exs
+++ 06/06.exs
@@ -1,4 +1,19 @@
 defmodule MemoryBanks do
+  def count_cycles(blocks) do
+    blocks
+    |> redistributions
+    |> Enum.reduce_while(%{}, fn
+      (redistribution, map) ->
+        case map[redistribution] do
+          nil ->
+            {:cont, Map.put_new(map, redistribution, :ok)}
+          _ ->
+            {:halt, Map.keys(map)}
+        end
+    end)
+    |> length
+  end
+
   def redistributions(blocks) do
     Stream.iterate(blocks, &redistribute/1)
   end
@@ -6,7 +21,7 @@ defmodule MemoryBanks do
   defp redistribute(blocks) do
     {max, max_index} = blocks
     |> Enum.with_index
-    |> Enum.max_by(fn {v, i} -> v end)
+    |> Enum.max_by(&elem(&1, 0))
     Stream.cycle([1])
     |> Enum.take(max)
     |> Enum.with_index
@@ -24,6 +39,11 @@ input = System.argv
 
 # Part one
 input
+|> MemoryBanks.count_cycles
+|> IO.inspect
+
+# Part two
+input
 |> MemoryBanks.redistributions
 |> Enum.reduce_while(%{}, fn
   (redistribution, map) ->
@@ -31,8 +51,8 @@ input
       nil ->
         {:cont, Map.put_new(map, redistribution, :ok)}
       _ ->
-        {:halt, Map.keys(map)}
+        {:halt, redistribution}
     end
 end)
-|> length
+|> MemoryBanks.count_cycles
 |> IO.inspect
