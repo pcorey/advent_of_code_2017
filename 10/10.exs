fe4ace13800c8d1c6e96b80b53f9950c2ed091c8
Pete Corey <petecorey@gmail.com>
2017-12-10 22:14:47 -0500

    Part 2

--- 10/10.exs
+++ 10/10.exs
@@ -1,15 +1,9 @@
-defmodule KnotHash do
-  def hash(input) do
-    input
-    |> parse
-    |> Enum.reduce({0, 0, Enum.to_list(0..255)}, &knot/2)
-    |> elem(2)
-  end
+require Bitwise
 
-  defp parse(input) do
+defmodule KnotHash do
+  def hash(input, index \\ 0, skip \\ 0, string \\ Enum.to_list(0..255)) do
     input
-    |> String.split(~r/,\s*/)
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.reduce({index, skip, string}, &knot/2)
   end
 
   def knot(length, {index, skip, string}) do
@@ -29,7 +23,30 @@ input = System.argv
 
 # Part one
 input
+|> String.split(~r/,\s*/)
+|> Enum.map(&String.to_integer/1)
 |> KnotHash.hash
+|> elem(2)
 |> Enum.take(2)
 |> (fn [a, b] -> a * b end).()
+|> IO.inspect
+
+# Part two
+key = input
+|> String.to_charlist
+|> Enum.concat([17, 31, 73, 47, 23])
+sparse_hash = Enum.reduce(1..64, {0, 0, 0..255 |> Enum.to_list}, fn
+  (n, {index, skip, out}) ->
+    KnotHash.hash(key, index, skip, out)
+end)
+|> elem(2)
+
+sparse_hash
+|> Enum.chunk_every(16)
+|> Enum.map(fn
+  chunk ->
+    Enum.reduce(chunk, &Bitwise.bxor/2)
+end)
+|> :erlang.list_to_binary
+|> Base.encode16(case: :lower)
 |> IO.inspect
