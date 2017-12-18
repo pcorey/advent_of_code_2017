9b375004463feb24f0a1e6424fa8355ca54561ad
Pete Corey <petecorey@gmail.com>
2017-12-18 14:18:46 -0500

    Part 2

--- 18/18.exs
+++ 18/18.exs
@@ -5,57 +5,96 @@ defmodule Duet do
     |> Enum.map(&String.split/1)
   end
 
-  def first_rcv(instructions) do
-    first_rcv(instructions, 0, %{})
+  def execute(instructions) do
+    execute(instructions,
+            {0, %{"p" => 0, rcv: [], snd: []}},
+            {0, %{"p" => 1, rcv: [], snd: []}})
   end
 
-  def first_rcv(_, _, %{rcv: rcv}), do: rcv
-  def first_rcv(instructions, i, mem) do
+  def execute(_, {_, %{halt: true}}, {_, %{halt: true}, snd: snd}) do
+    length(snd)
+  end
+
+  def execute(_, {_, %{wait: true, rcv: []}}, {_, %{wait: true, rcv: [], snd: snd}}) do
+    length(snd)
+  end
+
+  def execute(instructions, {i0, mem0}, {i1, mem1}) do
+    {i0, mem0, mem1} = execute_one(instructions, i0, mem0, mem1)
+    {i1, mem1, mem0} = execute_one(instructions, i1, mem1, mem0)
+    execute(instructions, {i0, mem0}, {i1, mem1})
+  end
+
+  def execute_one(_, i, mem0 = %{halt: true}, mem1) do
+    {i, mem0, mem1}
+  end
+
+  def execute_one(instructions, i, mem0, mem1) when i < 0 or i >= length(instructions) do
+    {i, Map.put(mem0, :halt, true), mem1}
+  end
+
+  def execute_one(instructions, i, mem0, mem1) do
     [instruction | args] = Enum.at(instructions, i)
     instruction = String.to_atom(instruction)
-    {i, mem} = apply(__MODULE__, instruction, [mem , i | args])
-    first_rcv(instructions, i, mem)
+    apply(__MODULE__, instruction, [mem0, mem1, i | args])
   end
 
-  def snd(mem, i, a) do
-    {i + 1, Map.put(mem, :snd, get_value(mem, a))}
+  # Part one:
+  # def first_rcv(instructions) do
+  #   first_rcv(instructions, 0, %{})
+  # end
+
+  # def first_rcv(_, _, %{rcv: rcv}), do: rcv
+  # def first_rcv(instructions, i, mem) do
+  #   [instruction | args] = Enum.at(instructions, i)
+  #   instruction = String.to_atom(instruction)
+  #   {i, mem} = apply(__MODULE__, instruction, [mem , i | args])
+  #   first_rcv(instructions, i, mem)
+  # end
+
+  def snd(mem0, mem1, i, a) do
+    v = get_value(mem0, a)
+    {i + 1, Map.update!(mem0, :snd, &(&1 ++ [v])), Map.update!(mem1, :rcv, &(&1 ++ [v]))}
   end
 
-  def set(mem, i, a, b) do
-    {i + 1, Map.put(mem, a, get_value(mem, b))}
+  def set(mem0, mem1, i, a, b) do
+    {i + 1, Map.put(mem0, a, get_value(mem0, b)), mem1}
   end
 
-  def add(mem, i, a, b) do
-    {i + 1, Map.put(mem, a, get_value(mem, a) + get_value(mem, b))}
+  def add(mem0, mem1, i, a, b) do
+    {i + 1, Map.put(mem0, a, get_value(mem0, a) + get_value(mem0, b)), mem1}
   end
 
-  def mul(mem, i, a, b) do
-    {i + 1, Map.put(mem, a, get_value(mem, a) * get_value(mem, b))}
+  def mul(mem0, mem1, i, a, b) do
+    {i + 1, Map.put(mem0, a, get_value(mem0, a) * get_value(mem0, b)), mem1}
   end
 
-  def mod(mem, i, a, b) do
-    {i + 1, Map.put(mem, a, rem(get_value(mem, a), get_value(mem, b)))}
+  def mod(mem0, mem1, i, a, b) do
+    {i + 1, Map.put(mem0, a, rem(get_value(mem0, a), get_value(mem0, b))), mem1}
   end
 
-  def rcv(mem, i, a) do
-    case Map.get(mem, :snd) do
-      0 -> {i + 1, mem}
-      b ->
+  def rcv(mem0, mem1, i, a) do
+    case Map.get(mem0, :rcv) do
+      [] ->
+        {i, Map.put(mem0, :wait, true), mem1}
+      [b | rest] ->
         {
           i + 1,
-          mem
+          mem0
           |> Map.put(a, b)
-          |> Map.put_new_lazy(:rcv, fn -> b end)
+          |> Map.put(:wait, false)
+          |> Map.put(:rcv, rest),
+          mem1
         }
     end
   end
 
-  def jgz(mem, i, a, b) do
-    case Map.get(mem, a) > 0 do
+  def jgz(mem0, mem1, i, a, b) do
+    case Map.get(mem0, a) > 0 do
       true ->
-        {i + get_value(mem, b), mem}
+        {i + get_value(mem0, b), mem0, mem1}
       false ->
-        {i + 1, mem}
+        {i + 1, mem0, mem1}
     end
   end
 
@@ -73,5 +112,5 @@ instructions = System.argv
 
 # Part one
 instructions
-|> Duet.first_rcv
+|> Duet.execute
 |> IO.inspect
